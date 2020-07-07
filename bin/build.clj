ca3304ed0c511c404a05d8d9a30c2962e16dabd7
Jeremy Schoffen <jschoffen.dev@gmail.com>
2020-07-07 16:37:45 +0200

    Added some default version-file generation.

--- bin/build.clj
+++ bin/build.clj
@@ -1,82 +1,34 @@
 (ns build
   (:require
     [clojure.spec.test.alpha :as spec-test]
-    [com.jeremyschoffen.java.nio.alpha.file :as fs]
     [com.jeremyschoffen.mbt.alpha.core :as mbt-core]
     [com.jeremyschoffen.mbt.alpha.default :as mbt-defaults]
+    [com.jeremyschoffen.mbt.alpha.default.versioning :as v]
     [com.jeremyschoffen.mbt.alpha.utils :as u]))
 
 
 (spec-test/instrument)
 
+(def specific-conf
+  (sorted-map
+    :project/working-dir (u/safer-path)
+    :versioning/scheme mbt-defaults/simple-scheme
+    :versioning/major :alpha
+    :project/author "Jeremy Schoffen"
+    :version-file/ns 'com.jeremyschoffen.mbt.alpha.version
+    :version-file/path (u/safer-path "src" "com" "jeremyschoffen" "mbt" "alpha" "version.clj")))
 
-(def conf (-> (sorted-map
-                :project/working-dir (u/safer-path)
-                :versioning/scheme mbt-defaults/simple-scheme
-                :project/author "Jeremy Schoffen"
-                :version-file/ns 'com.jeremyschoffen.mbt.alpha.version
-                :version-file/path (u/safer-path "src" "com" "jeremyschoffen" "mbt" "alpha" "version.clj"))
+(def conf (-> specific-conf
               mbt-defaults/make-conf))
 
 
-;;----------------------------------------------------------------------------------------------------------------------
-;; utils
-;;----------------------------------------------------------------------------------------------------------------------
-(defn next-version [param]
-  (let [current-version (mbt-defaults/current-version param)]
-    (if-not current-version
-      (mbt-defaults/initial-version param)
-      (-> param
-          (assoc :versioning/version
-                 (update current-version :distance inc))
-          mbt-defaults/bump-version))))
-
-
-(defn write-version-file! [param]
+(defn new-milestone! [param]
   (-> param
-      (u/assoc-computed :project/version (comp str next-version))
-      (mbt-defaults/write-version-file!)))
-
-
-(defn git-add-version-file! [{p :version-file/path
-                              repo :git/repo}]
-  (mbt-core/git-add! {:git/repo repo
-                      :git/add! {:git.add!/file-patterns [(->> p
-                                                               (fs/relativize repo)
-                                                               str)]}}))
-
-(defn git-commit-version-file! [param]
-  (mbt-core/git-commit! (assoc param
-                          :git/commit! {:git.commit/message "Committed version file."})))
-
-
-(defn add-version-file! [ctxt]
-  (-> ctxt
-      (u/side-effect! write-version-file!)
-      (u/side-effect! git-add-version-file!)
-      (u/side-effect! git-commit-version-file!)))
-
-;;----------------------------------------------------------------------------------------------------------------------
-;; Build
-;;----------------------------------------------------------------------------------------------------------------------
-(defn tag-new-version! [param]
-  (-> param
-      (u/check mbt-defaults/check-repo-in-order)
-      (u/side-effect! add-version-file!)
+      (u/side-effect! mbt-defaults/add-version-file!)
       (u/side-effect! mbt-defaults/bump-tag!)))
 
 
-(defn build! [conf]
-  (-> conf
-      (u/assoc-computed :project/version (comp str mbt-defaults/current-version))
-      mbt-defaults/ensure-jar-defaults
-      mbt-defaults/jar!))
-
-
 (comment
-  (str (mbt-defaults/current-version conf))
-  (str (next-version conf))
-  (tag-new-version! conf)
-
+  (new-milestone! conf)
   (mbt-core/clean! conf)
-  (build! conf))
+  (mbt-defaults/build-jar! conf))
--- src/com/jeremyschoffen/mbt/alpha/default.clj
+++ src/com/jeremyschoffen/mbt/alpha/default.clj
@@ -2,26 +2,32 @@
   (:require
     [com.jeremyschoffen.mbt.alpha.default.building :as building]
     [com.jeremyschoffen.mbt.alpha.default.defaults :as defaults]
+    [com.jeremyschoffen.mbt.alpha.default.tasks :as tasks]
     [com.jeremyschoffen.mbt.alpha.default.versioning :as versioning]
     [com.jeremyschoffen.mbt.alpha.utils :as u]))
 
-
+;;----------------------------------------------------------------------------------------------------------------------
+;; Default conf
+;;----------------------------------------------------------------------------------------------------------------------
 (u/alias-fn make-conf defaults/make-context)
 
+;;----------------------------------------------------------------------------------------------------------------------
+;; Versioning schemes
+;;----------------------------------------------------------------------------------------------------------------------
 (def maven-scheme versioning/maven-scheme)
 (def semver-scheme versioning/semver-scheme)
 (def simple-scheme versioning/simple-scheme)
 
-(u/alias-fn current-version versioning/current-version)
-(u/alias-fn initial-version versioning/schemes-initial-version)
-(u/alias-fn bump-version versioning/schemes-bump)
-(u/alias-fn next-version versioning/next-version)
-(u/alias-fn check-repo-in-order versioning/check-repo-in-order)
+
+;;----------------------------------------------------------------------------------------------------------------------
+;; Versioning machinery
+;;----------------------------------------------------------------------------------------------------------------------
+;; Git versioning
 (u/alias-fn bump-tag! versioning/bump-tag!)
 
-(u/alias-fn make-version-file versioning/make-version-file)
-(u/alias-fn write-version-file! versioning/write-version-file!)
-
-(u/alias-fn ensure-jar-defaults building/ensure-jar-defaults)
-(u/alias-fn jar! building/jar!)
-(u/alias-fn uberjar! building/uberjar!)
+;;----------------------------------------------------------------------------------------------------------------------
+;; Premade
+;;----------------------------------------------------------------------------------------------------------------------
+(u/alias-fn add-version-file! tasks/add-version-file!)
+(u/alias-fn build-jar! tasks/jar!)
+(u/alias-fn build-uberjar! tasks/uberjar!)
