8470b1ea94c80202bf62ff5e3348e0d4c0643150
Jeremy Schoffen <jschoffen.dev@gmail.com>
2020-07-21 15:16:09 +0200

    Added another search for namespaces to compile and some doc.

--- src/com/jeremyschoffen/mbt/alpha/core/building/compilation.clj
+++ src/com/jeremyschoffen/mbt/alpha/core/building/compilation.clj
@@ -4,7 +4,9 @@
     [com.jeremyschoffen.java.nio.alpha.file :as fs]
 
     [com.jeremyschoffen.mbt.alpha.core.specs]
-    [com.jeremyschoffen.mbt.alpha.utils :as u]))
+    [com.jeremyschoffen.mbt.alpha.utils :as u])
+  (:import
+    [java.util.jar JarFile]))
 
 
 ;; inspired by https://github.com/luchiniatwork/cambada/blob/master/src/cambada/compile.clj
@@ -16,7 +18,10 @@
         dirs))
 
 (defn project-nss
-  "Find all namespace from a classpath located inside the working directory."
+  "Use an indexed classpath to find all Clojure namespaces from src directories located inside the working directory.
+
+  See:
+    - `com.jeremyschoffen.mbt.alpha.core.building.classpath/indexed-classpath`"
   [{cp :classpath/index}]
   (-> cp :classpath/dir dirs->nss))
 
@@ -26,7 +31,11 @@
 
 
 (defn external-nss
-  "Find all namespace from a classpath located outside the working directory."
+  "Use an indexed classpath to find all Clojure namespaces from src directories located outside the working directory.
+  These would be namespaces from local deps or directly from a git repo.
+
+  See:
+    - `com.jeremyschoffen.mbt.alpha.core.building.classpath/indexed-classpath`"
   [{cp :classpath/index}]
   (-> cp :ext-dep dirs->nss))
 
@@ -35,10 +44,28 @@
            :ret :clojure.compilation/namespaces)
 
 
+(defn jar-nss [{cp :classpath/index}]
+  "Use an indexed classpath to find all Clojure namespaces from jars.
+
+  See:
+    - `com.jeremyschoffen.mbt.alpha.core.building.classpath/indexed-classpath`"
+  (into []
+        (comp
+          (map #(JarFile. ^String %))
+          (mapcat ns-find/find-namespaces-in-jarfile))
+        (:classpath/jar cp)))
+
+(u/spec-op jar-nss
+           :param {:req [:classpath/index]}
+           :ret :clojure.compilation/namespaces)
+
 ;; TODO: would be nice to have something like boot's pods to isolate
 ;; the current environment while compiling.
-(defn compile! [{output-dir :clojure.compilation/output-dir
-                 namespaces :clojure.compilation/namespaces}]
+(defn compile!
+  "Compile a list of namespaces provided under the key `:clojure.compilation/namespaces`, the results are placed at the
+  location specified under the key `:clojure.compilation/output-dir`."
+  [{output-dir :clojure.compilation/output-dir
+    namespaces :clojure.compilation/namespaces}]
   (fs/create-directories! output-dir)
   (binding [*compile-path* output-dir]
     (doseq [n namespaces]
@@ -48,6 +75,11 @@
            :param {:req [:clojure.compilation/output-dir
                          :clojure.compilation/namespaces]})
 
+(comment
+  (require '[com.jeremyschoffen.mbt.alpha.core.building.deps :as deps])
+  (require '[com.jeremyschoffen.mbt.alpha.core.building.classpath :as cp])
 
-
-
+  (-> {:project/working-dir (u/safer-path)}
+      (u/assoc-computed :project/deps deps/get-deps
+                        :classpath/index cp/indexed-classpath)
+      jar-nss))
